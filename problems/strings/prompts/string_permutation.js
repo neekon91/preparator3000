98038e2be1a5146a2cac58f1f050df25a20dcc41
Neekon <neekon91@gmail.com>
2016-10-30 21:51:29 -0700

    perms are fun

--- problems/strings/prompts/string_permutation.js
+++ problems/strings/prompts/string_permutation.js
@@ -1,7 +1,7 @@
 /**
   * A permutation is an arrangement of members of a set.
   * https://en.wikipedia.org/wiki/Permutation
-  * 
+  *
   * Implement stringPermutation so that it takes two string inputs, and outputs
   * a boolean indicating if on is a permutation of the other. All letters must be included.
   *
@@ -10,8 +10,14 @@
   * stringPermutation('what', 'wat') // false
   */
 
-const stringPermutation = function(string1, string2) {
-
+const stringPermutation = (string1, string2, outputs = []) => {
+  (sub = (left, combo)=>{
+    if (left.length < 1) return outputs.push(combo)
+    left.map((val, i)=>{
+      sub(left.slice(0,i).concat(left.slice(i + 1)), combo + val)
+    })
+  })([...string1], '')
+  return outputs.includes(string2);
 };
 
 module.exports = { stringPermutation };
