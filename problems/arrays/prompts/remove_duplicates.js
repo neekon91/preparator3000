6530872690912e4ba8b73b2866004747a081bf57
Neekon <neekon91@gmail.com>
2016-10-30 21:11:27 -0700

    dupes

--- problems/arrays/prompts/remove_duplicates.js
+++ problems/arrays/prompts/remove_duplicates.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // returns an array without duplicates
-const removeDuplicates = (array) => {
+const removeDuplicates = array =>
   // your code here
-  return new Set(array).toJSON()
-};
+  Object.keys(array.reduce((memo, val)=>{
+    memo[val] = val;
+    return memo;
+  },{})).map(val => Number(val));
 
 module.exports = { removeDuplicates };
