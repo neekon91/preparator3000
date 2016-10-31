dc5ff47e173f472fa1684b713fb9956838ef1294
Neekon <neekon91@gmail.com>
2016-10-30 21:31:44 -0700

    some

--- problems/arrays/prompts/some.js
+++ problems/arrays/prompts/some.js
@@ -1,8 +1,9 @@
 'use strict';
 
 // determine if some elements in the array pass a truth test
-const some = (array, truthTest) => {
+const some = (array, truthTest) =>
   // your code here
-};
+  array.reduce((memo, val)=>
+    (!memo && truthTest(val)) ? true : memo, false);
 
 module.exports = { some };
