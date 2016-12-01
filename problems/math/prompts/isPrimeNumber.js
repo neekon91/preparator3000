c23435014549695755a98b8613a36fa00bd898d3
Neekon <neekon91@gmail.com>
2016-11-30 16:10:29 -0800

    prime

--- problems/math/prompts/isPrimeNumber.js
+++ problems/math/prompts/isPrimeNumber.js
@@ -1,12 +1,16 @@
 /**
   * A prime number is a number that is only divisible by 1 and itself.
   *
-  * Implement isPrimeNumber to check an input number and return a 
-  * boolean indicating if it is prime or not. 
+  * Implement isPrimeNumber to check an input number and return a
+  * boolean indicating if it is prime or not.
   */
 
 const isPrimeNumber = number => {
-
+  if(number < 2) return false;
+  for(let i = 2; i <= Math.sqrt(number); i++){
+    if(number % i === 0) return false;
+  }
+  return true;
 };
 
 module.exports = { isPrimeNumber };
